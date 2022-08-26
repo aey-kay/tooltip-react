a81157f9b8b74ec48b0d7528313aeb16c0ff5433
Amit Sinha <aamitsinha456@gmail.com>
2022-08-26 17:08:33 +0530

    Updates

---
+++ precache-manifest.e5a8f43bd4beda744d2163dc7085bfb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb97b067dec23aa0198dbd427a5c0e22",
+    "url": "/tooltip-react/index.html"
+  },
+  {
+    "revision": "5ded614936a074ac8d23",
+    "url": "/tooltip-react/static/css/main.6514b057.chunk.css"
+  },
+  {
+    "revision": "abbb1af2e914eedbde2a",
+    "url": "/tooltip-react/static/js/2.ada14da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip-react/static/js/2.ada14da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ded614936a074ac8d23",
+    "url": "/tooltip-react/static/js/main.34d29af0.chunk.js"
+  },
+  {
+    "revision": "cc4e1c19c22b3fd61136",
+    "url": "/tooltip-react/static/js/runtime-main.253b4ab2.js"
+  }
+]);
